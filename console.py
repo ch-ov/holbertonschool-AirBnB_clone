edf6c8094dc1f6a772ee2cc3b6950d939c234b5b
ajmarrugos <ajmarrugos@gmail.com>
2022-07-02 16:17:54 -0500

    Created first input conditionals to simulate commands

--- console.py
+++ console.py
@@ -1,8 +1,34 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """ Main connsole for the system app """
 
-path = 'C:/'
+import os
 
-print("AirBnB Clone: Basic Console v1.00")
+cmd_list = ["help", "quit", "EOF"]
+
+def welcome_menu():
+    """ Displays a Welcome menu for start """
+    print("""+------------------------------------+
+|    Welcome to the HBNB Console!    |
++------------------------------------+
+    """)
+
+def help_menu():
+    """ Displays a dictionary of Commands """
+    print("""
+Documented commands (type help <topic>):
+========================================""")
+    print(dict)
+
+welcome_menu()
+
+dict = ["help", "info", "contact"]
+
 while True:
-    cli = input(">>> ")
+    cmd = input("(hbnb) >>> ")
+    if cmd == 'help':
+        help_menu()
+    if cmd == 'quit':
+        exit()
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
