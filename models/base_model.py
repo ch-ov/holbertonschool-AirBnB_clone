ebedd0d534d0c23053f1b5d6ba71682c73326c28
christian <ch.ov@outlook.com>
2022-07-03 14:58:30 +0000

    AUTHORS && 3

--- models/base_model.py
+++ models/base_model.py
@@ -3,10 +3,21 @@
 from uuid import uuid4
 from datetime import datetime
 
+
 class BaseModel:
-    id = str(uuid.uuid4())
+    id = str(uuid4())
     created_at = datetime.now()
     updated_at = datetime.now()
-def __str__(self):
-    print (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
 
+    def __str__(self):
+        return(f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        dictionary = dict(self.__dict__)
+        dictionary.update({"__class__": self.__class__.__name__,
+                           "created_at": str(((self.created_at).isoformat())),
+                           "updated_at": str(((self.updated_at).isoformat()))})
+        return dictionary
