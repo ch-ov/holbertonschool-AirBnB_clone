38b6a272683dad4daffb5e5dd5ca4b342200095b
ajmarrugos <ajmarrugos@gmail.com>
2022-07-04 14:41:45 -0500

    Implemented methods to interpret empty line & EOF commands

--- console.py
+++ console.py
@@ -1,15 +1,27 @@
 #!/usr/bin/python3
-""" Main connsole for the system app """
+"""Main connsole for the system app"""
 
 import cmd
 
 
 class HBNBCommand(cmd.Cmd):
-    """ Class initialized as Command Prompt with module 'cmd' """
+    """Class initialized as Command Prompt with module 'cmd'"""
 
     prompt = "(hbnb) >>> "
 
+    def do_quit(self, arg):
+        """Quit Command to exit the Console"""
+        exit(0)
+
+    def do_EOF(self, arg):
+        """Quit Command to exit the Console"""
+        exit(0)
+
+    def emptyline(self, arg):
+        """Executes when an empty line is found in the prompt"""
+        pass
+
 
 if __name__ == '__main__':
-    """ Stops other modules from starting before this main program """
+    """Stops other modules from starting before this main program"""
     HBNBCommand().cmdloop()
