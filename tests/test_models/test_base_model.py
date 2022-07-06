c9b3baaff5914ee2d92f11d230881b78a52883a7
christian <ch.ov@outlook.com>
2022-07-06 23:23:37 +0000

    test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,8 +1,8 @@
 """test module BaseModel"""
+
 import unittest
-from models import storage
-from models.base_model import BaseModel
-from datetime import datetime
+import models
+import datetime
 
 
 class Test_Base_Model(unittest.TestCase):
@@ -11,6 +11,19 @@ class Test_Base_Model(unittest.TestCase):
     def test_not_run(self):
         """test for method test_not_run"""
         pass
+
+    def test_pep8_conformance_file_storage(self):
+        """
+        Method that tests:
+            if a file meet with pep8 criteria
+        """
+        style = pep8.StyleGuide()
+        check = style.check_files(['models/base_model.py'])
+        self.assertEqual(
+            check.total_errors,
+            0,
+            'PEP8 style errors: %d' % check.total_errors
+        )
 
 
 if __name__ == "__main__":
