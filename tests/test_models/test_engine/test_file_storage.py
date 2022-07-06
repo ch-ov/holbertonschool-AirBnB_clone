470c0d0e7b3da4d5b039e706eb30f2e67a83226c
christian <ch.ov@outlook.com>
2022-07-06 02:39:37 +0000

    test_file_storage.py

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,11 +1,12 @@
 import unittest
 from models import storage
 from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
 from datetime import datetime
 
 
-class Test_Base_Model(unittest.TestCase):
-    """test for class BaseModel"""
+class Test_File_Storage(unittest.TestCase):
+    """test for class File Storage"""
 
     def test_not_run(self):
         """test for method test_not_run"""
