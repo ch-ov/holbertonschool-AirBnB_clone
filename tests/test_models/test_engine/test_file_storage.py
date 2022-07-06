dc7d6070d58cb19e92ca1b74d6a04146aceecf13
christian <ch.ov@outlook.com>
2022-07-06 02:28:21 +0000

    test_engine/

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,16 @@
+import unittest
+from models import storage
+from models.base_model import BaseModel
+from datetime import datetime
+
+
+class Test_Base_Model(unittest.TestCase):
+    """test for class BaseModel"""
+
+    def test_not_run(self):
+        """test for method test_not_run"""
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
