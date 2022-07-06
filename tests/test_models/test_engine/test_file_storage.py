8d458c08c0fe2dd7d54f007a73f560cd59cee929
christian <ch.ov@outlook.com>
2022-07-06 23:35:52 +0000

    test_file_storage.py

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -12,6 +12,19 @@ class Test_File_Storage(unittest.TestCase):
         """test for method test_not_run"""
         pass
 
+    def test_pep8_conformance_file_storage(self):
+        """
+        Method that tests:
+            if a file meet with pep8 criteria
+        """
+        style = pep8.StyleGuide()
+        check = style.check_files(['models/base_model.py'])
+        self.assertEqual(
+            check.total_errors,
+            0,
+            'PEP8 style errors: %d' % check.total_errors
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
