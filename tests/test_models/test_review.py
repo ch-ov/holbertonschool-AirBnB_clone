754b4d095dd734795baee6127fc8fe1b71c98513
christian <ch.ov@outlook.com>
2022-07-06 23:43:10 +0000

    tests

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -9,6 +9,23 @@ import datetime
 class TestReview(unittest.TestCase):
     """test Review class"""
 
+    def test_not_run(self):
+        """test for method test_not_run"""
+        pass
+
+    def test_pep8_conformance_file_storage(self):
+        """
+        Method that tests:
+            if a file meet with pep8 criteria
+        """
+        style = pep8.StyleGuide()
+        check = style.check_files(['models/base_model.py'])
+        self.assertEqual(
+            check.total_errors,
+            0,
+            'PEP8 style errors: %d' % check.total_errors
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
