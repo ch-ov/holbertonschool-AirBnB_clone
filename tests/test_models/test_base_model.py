22b4d1b41e0ab24abeddf19861b701459cc084b2
christian <ch.ov@outlook.com>
2022-07-08 18:19:16 +0000

    test_base_model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -33,6 +33,46 @@ class Test_Base_Model(unittest.TestCase):
         self.assertIsNotNone(models.base_model.BaseModel.save.__doc__)
         self.assertIsNotNone(models.base_model.BaseModel.to_dict.__doc__)
 
+    def test_class(self):
+        """test instance class"""
+        instance = models.base_model.BaseModel()
+        self.assertIsInstance(instance, models.base_model.BaseModel)
+
+    def test_type(self):
+        """test type of instance atributes"""
+        instance = models.base_model.BaseModel()
+        self.assertIsInstance(instance.id, str)
+        self.assertIsInstance(instance.created_at, datetime.datetime)
+        self.assertIsInstance(instance.updated_at, datetime.datetime)
+
+    def test_init(self):
+        """test type of instance atributes"""
+        instance = models.base_model.BaseModel()
+        self.assertIsInstance(instance.id, str)
+        self.assertIsInstance(instance.created_at, datetime.datetime)
+        self.assertIsInstance(instance.updated_at, datetime.datetime)
+
+    def test_str(self):
+        """test __str__ method"""
+        instance = models.base_model.BaseModel()
+        string = "[BaseModel] ({}) {}".format(instance.id, instance.__dict__)
+        self.assertEqual(string, str(instance))
+
+    def test_save(self):
+        """test save method"""
+        instance = models.base_model.BaseModel()
+        date = instance.updated_at
+        instance.save()
+        self.assertLess(date, instance.updated_at)
+
+    def test_to_dict(self):
+        """test to_dict method"""
+        instance = models.base_model.BaseModel()
+        dictionary = instance.to_dict()
+        self.assertIsInstance(dictionary, dict)
+        self.assertEqual(instance.__class__.__name__, dictionary["__class__"])
+        self.assertEqual(instance.id, dictionary["id"])
+
 
 if __name__ == "__main__":
     unittest.main()
