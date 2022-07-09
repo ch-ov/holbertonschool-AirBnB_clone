2e8fd66d23424ce213bd5f78adfed1e1ffa4c866
ajmarrugos <ajmarrugos@gmail.com>
2022-07-08 20:30:42 -0500

    Optimized User model: Unnecesary imports

--- models/user.py
+++ models/user.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """First User class module"""
+
 from models.base_model import BaseModel
-import models
 
 
 class User(BaseModel):
@@ -11,7 +11,3 @@ class User(BaseModel):
     password = ""
     first_name = ""
     last_name = ""
-
-    def __init__(self, *args, **kwargs):
-        """class constructor that initializes the class"""
-        super().__init__(*args, **kwargs)
