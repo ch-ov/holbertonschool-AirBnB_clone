04458d7fb7badc5a8c0ca93068fdab83383c30b6
christian <ch.ov@outlook.com>
2022-07-09 02:21:52 +0000

    user.py

--- models/__init__.py
+++ models/__init__.py
@@ -2,5 +2,6 @@
 """create a unique FileStorage instance for application"""
 from models.engine.file_storage import FileStorage
 
+
 storage = FileStorage()
 storage.reload()
--- models/user.py
+++ models/user.py
@@ -2,6 +2,7 @@
 """First User class module"""
 from models.base_model import BaseModel
 
+
 class User(BaseModel):
     """class User that inherits from BaseModel"""
 
